60111b791cd0f373dd857e4cc30f0357575ea934
santiago vides <santivides01@gmail.com>
2025-01-09 22:30:52 -0500

    barra de navegacion agregada

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -13,39 +13,65 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset('assets/images/bridgecare_logo.png',
-            fit: BoxFit.contain, height: 100),
-        centerTitle: true,
-        backgroundColor: const Color(0xFF0F0147),
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(bottom: Radius.circular(15))),
-        toolbarHeight: 100,
-      ),
-      body: Column(
-        children: [
-          GridView.count(
-            padding: const EdgeInsets.all(20),
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            shrinkWrap: true,
-            children: imagenes.map((path) {
-              return ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.asset(path, fit: BoxFit.cover));
-            }).toList(),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
+        appBar: AppBar(
+          title: Image.asset('assets/images/bridgecare_logo.png',
+              fit: BoxFit.contain, height: 200),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF0F0147),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
+          toolbarHeight: 200,
+        ),
+        body: Column(
+          children: [
+            GridView.count(
+              padding: const EdgeInsets.all(20),
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              shrinkWrap: true,
+              children: imagenes.map((path) {
+                return ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(path, fit: BoxFit.cover));
+              }).toList(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
                 'Bienvenido a BridgeCare, la aplicación para el diagnostico del estado de un puente',
                 style: TextStyle(
                     fontStyle: FontStyle.normal,
-                    color: Colors.black.withOpacity(1))),
-          )
-        ],
-      ),
-    );
+                    color: Colors.black,
+                    fontSize: 17),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.shifting,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.home,
+                      color: Color(0xFFC5C5C5), size: 50),
+                  label: 'home'),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.search,
+                      color: Color(0xFFC5C5C5), size: 50),
+                  label: 'home'),
+              BottomNavigationBarItem(
+                  icon:
+                      const Icon(Icons.add, color: Color(0xFFF29E23), size: 50),
+                  label: 'home'),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.notifications,
+                      color: Color(0xFFC5C5C5), size: 50),
+                  label: 'home'),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.person,
+                      color: Color(0xFFC5C5C5), size: 50),
+                  label: 'home')
+            ]));
   }
 }
