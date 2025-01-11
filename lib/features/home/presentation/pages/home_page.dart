4bb2c6115b47d75415d83ff5cc45ac7c76bb24d2
santiago vides <santivides01@gmail.com>
2025-01-10 19:11:45 -0500

    imagenes agregadas

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -4,10 +4,10 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   static const List<String> imagenes = [
-    'assets/images/puente-inicio.png',
-    'assets/images/puente-inicio.png',
-    'assets/images/puente-inicio.png',
-    'assets/images/puente-inicio.png'
+    'assets/images/puente-inicio.jpeg',
+    'assets/images/puente-inicio.jpeg',
+    'assets/images/puente-inicio.jpeg',
+    'assets/images/puente-inicio.jpeg'
   ];
 
   @override
@@ -15,14 +15,15 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Image.asset('assets/images/bridgecare_logo.png',
-              fit: BoxFit.contain, height: 200),
+              fit: BoxFit.contain, height: 150),
           centerTitle: true,
           backgroundColor: const Color(0xFF0F0147),
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
-          toolbarHeight: 200,
+          toolbarHeight: 150,
         ),
-        body: Column(
+        body: SingleChildScrollView(
+            child: Column(
           children: [
             GridView.count(
               padding: const EdgeInsets.all(20),
@@ -33,7 +34,11 @@ class HomePage extends StatelessWidget {
               children: imagenes.map((path) {
                 return ClipRRect(
                     borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(path, fit: BoxFit.cover));
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Image.asset(path, fit: BoxFit.cover),
+                    ));
               }).toList(),
             ),
             Padding(
@@ -48,17 +53,17 @@ class HomePage extends StatelessWidget {
               ),
             )
           ],
-        ),
+        )),
         bottomNavigationBar: BottomNavigationBar(
             type: BottomNavigationBarType.shifting,
             items: <BottomNavigationBarItem>[
               BottomNavigationBarItem(
                   icon: const Icon(Icons.home,
-                      color: Color(0xFFC5C5C5), size: 50),
+                      color: Color(0xFFC5C5C5), size: 40),
                   label: 'home'),
               BottomNavigationBarItem(
                   icon: const Icon(Icons.search,
-                      color: Color(0xFFC5C5C5), size: 50),
+                      color: Color(0xFFC5C5C5), size: 40),
                   label: 'home'),
               BottomNavigationBarItem(
                   icon:
@@ -66,12 +71,25 @@ class HomePage extends StatelessWidget {
                   label: 'home'),
               BottomNavigationBarItem(
                   icon: const Icon(Icons.notifications,
-                      color: Color(0xFFC5C5C5), size: 50),
+                      color: Color(0xFFC5C5C5), size: 40),
                   label: 'home'),
               BottomNavigationBarItem(
                   icon: const Icon(Icons.person,
-                      color: Color(0xFFC5C5C5), size: 50),
+                      color: Color(0xFFC5C5C5), size: 40),
                   label: 'home')
             ]));
   }
 }
+
+// GridView.count(
+//               padding: const EdgeInsets.all(20),
+//               crossAxisCount: 2,
+//               crossAxisSpacing: 10,
+//               mainAxisSpacing: 10,
+//               shrinkWrap: true,
+//               children: imagenes.map((path) {
+//                 return ClipRRect(
+//                     borderRadius: BorderRadius.circular(10),
+//                     child: Image.asset(path, fit: BoxFit.cover));
+//               }).toList(),
+//             ),
