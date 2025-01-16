62b351c7b564ca675f0c8258b24ae39528363d0b
santiago vides <santivides01@gmail.com>
2025-01-15 22:37:19 -0500

    empezado el formulario de inventario y la clase de construccion de form separada

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:bridgecare/core/providers/theme_provider.dart';
 import 'package:bridgecare/core/widgets/navbar.dart';
 import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
+import 'package:bridgecare/shared/forms/form_Inventory.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
+        '/inventarioForm': (context) => FormInventory(),
         '/main': (context) => const BottomNavWrapper(),
       },
     );
