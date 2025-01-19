4a429f8fa4488d57259bcc02e628de033774b596
santiago vides <santivides01@gmail.com>
2025-01-19 16:52:26 -0500

    campos del funcionario hechos

--- lib/features/assets/presentation/pages/buildForm.dart
+++ lib/features/assets/presentation/pages/buildForm.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 
 class buildForm extends StatelessWidget {
   final String titulo;
-  final List<Widget> campos;
+  final List<SectionData> secciones;
 
-  const buildForm({required this.campos, required this.titulo});
+  const buildForm({required this.titulo, required this.secciones, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(16),
-      padding: EdgeInsets.only(bottom: 16),
+      padding: EdgeInsets.only(bottom: 10),
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(10)),
       child: Column(
@@ -21,9 +23,41 @@ class buildForm extends StatelessWidget {
             titulo,
             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           ),
-          ...campos
+          ...secciones.map((section) => _buildSection(section)).toList(),
         ],
       ),
     );
   }
+
+  Widget _buildSection(SectionData section) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.only(bottom: 16),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(section.tituloSeccion,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10)),
+          TextFormField(
+            controller: section.controller,
+            decoration: InputDecoration(
+                hintText: section.labelCampo, border: OutlineInputBorder()),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SectionData {
+  final String tituloSeccion;
+  final String labelCampo;
+  final TextEditingController controller;
+
+  SectionData(
+      {required this.tituloSeccion,
+      required this.labelCampo,
+      required this.controller});
 }
