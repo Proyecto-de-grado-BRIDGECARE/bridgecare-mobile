ba6893fa2c27ad31b4d2095109176d354d296fbe
Juanba2002 <bajuan00@gmail.com>
2025-02-02 17:00:34 -0500

    Formulario

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:bridgecare/core/providers/theme_provider.dart';
 import 'package:bridgecare/core/widgets/navbar.dart';
+import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
 import 'package:bridgecare/shared/forms/form_Inventory.dart';
+import 'package:bridgecare/shared/forms/form_inspection.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,7 +20,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginPage(),
-        '/inventarioForm': (context) => FormInventory(),
+        //'/inventarioForm': (context) => FormInventory(),
+        '/inspeccionForm': (context) => FormInspection(),
         '/main': (context) => const BottomNavWrapper(),
       },
     );
