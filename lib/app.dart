82bffdc4ce84f1eae24998f18db2a35e54fe632b
santiago vides <santivides01@gmail.com>
2025-02-04 15:57:17 -0500

    archivo de inspecciones movido

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
-import 'package:bridgecare/features/assets/presentation/inspections.dart';
+import 'package:bridgecare/features/Inspecciones/inspecciones.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
