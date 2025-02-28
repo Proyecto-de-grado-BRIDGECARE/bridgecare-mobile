320172e35f7503130c6d390dbe5124e365c1c102
santiago vides <santivides01@gmail.com>
2025-02-27 21:58:30 -0500

    elemento post creado para enviar la informacion

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
+import 'package:bridgecare/features/registroUsuario/registroUsuario.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,10 +10,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BridgeCare',
-      initialRoute: '/login',
+      initialRoute: '/registro',
       routes: {
-        '/login': (context) => LoginPage(),
+        // '/login': (context) => LoginPage(),
         //'/home': (context) => HomePage(),
+        '/registro': (context) => registroUsuario(),
       },
     );
   }
