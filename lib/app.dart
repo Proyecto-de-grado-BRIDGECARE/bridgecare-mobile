3deeb4af00fc7c1e743cfcb1fbf5f65d424c7a74
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-02-28 00:44:39 -0500

    Implement theme provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
+import 'package:bridgecare/core/providers/theme_provider.dart';
+import 'package:bridgecare/core/widgets/navbar.dart';
 import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,10 +11,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BridgeCare',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode:
+          Provider.of<ThemeProvider>(context).themeMode, // Use theme provider
       initialRoute: '/login',
       routes: {
-        '/login': (context) => LoginPage(),
-        //'/home': (context) => HomePage(),
+        '/login': (context) => const LoginPage(),
+        '/main': (context) => const BottomNavWrapper(),
       },
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:bridgecare/core/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:bridgecare/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
