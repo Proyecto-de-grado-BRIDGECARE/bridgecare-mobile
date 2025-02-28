64ea16d53dc93ddd60a3795b9b91c23a0dd22210
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-02-28 00:56:50 -0500

    Create empty search screen

---
+++ lib/features/search_bridge/presentation/search_bridge.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchBridgeScreen extends StatelessWidget {
+  const SearchBridgeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: const Text("Search for a Bridge Here!"),
+      ),
+    );
+  }
+}
