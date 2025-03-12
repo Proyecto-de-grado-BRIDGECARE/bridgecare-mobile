d1396b7f61ae203f7b0cc2b8ba837333dbc20436
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-03-12 15:07:01 -0500

    Fix classpath

--- lib/app.dart
+++ lib/app.dart
@@ -2,14 +2,13 @@ import 'package:bridgecare/core/providers/theme_provider.dart';
 import 'package:bridgecare/core/widgets/navbar.dart';
 import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
 import 'package:bridgecare/features/formularioInspeccion/form_inspection.dart';
+import 'package:bridgecare/features/formularioInventario/form_Inventario.dart';
 
 import 'package:bridgecare/features/registroUsuario/registroUsuario.dart';
-import 'package:bridgecare/shared/forms/form_inventory.dart';
 import 'package:bridgecare/features/modificarUsuarios/modificarUsuarios.dart';
 import 'package:bridgecare/features/listaUsuarios/listaUsuarios.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/registro',
       routes: {
         '/login': (context) => const LoginPage(),
-        '/inventarioForm': (context) => FormInventory(),
+        '/inventarioForm': (context) => formInventario(),
         '/inspeccionForm': (context) => FormInspection(),
         '/main': (context) => const BottomNavWrapper(),
         '/registro': (context) => registroUsuario(),
