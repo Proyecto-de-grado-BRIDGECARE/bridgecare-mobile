728361934d3f20698bdd30777e2ae905bec081fa
Daniel Vargas <dvargasp@javeriana.edu.co>
2025-03-13 02:10:57 -0500

    Routes file path & splash screen implementation

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,6 @@
+import 'package:bridgecare/config/app_routes.dart';
 import 'package:bridgecare/core/providers/theme_provider.dart';
-import 'package:bridgecare/core/widgets/navbar.dart';
-import 'package:bridgecare/features/auth/presentation/pages/login_page.dart';
-import 'package:bridgecare/features/bridge_management/inspection/presentation/pages/form_inspection.dart';
-import 'package:bridgecare/features/bridge_management/inventory/presentation/pages/form_inventario.dart';
-import 'package:bridgecare/features/user_management/create_user/presentation/pages/create_user.dart';
-import 'package:bridgecare/features/user_management/update_user/presentation/pages/update_user.dart';
-import 'package:bridgecare/features/user_management/read_user/presentation/pages/read_user.dart';
-import 'package:bridgecare/features/search_bridge/presentation/pages/search_inspection.dart';
+import 'package:bridgecare/shared/widgets/splash_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -21,17 +15,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       themeMode:
           Provider.of<ThemeProvider>(context).themeMode, // Use theme provider
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/inventarioForm': (context) => FormInventario(),
-        '/inspeccionForm': (context) => FormInspection(),
-        '/main': (context) => const BottomNavWrapper(),
-        '/registro': (context) => RegistroUsuario(),
-        '/modificarUsuario': (context) => ModificarUsuario(),
-        '/usuarios': (context) => ListaUsuarios(),
-        '/inspecciones': (context) => InspeccionesPage(),
-      },
+      home: const SplashToLoginTransition(),
+      routes: AppRoutes.routes,
     );
   }
 }
